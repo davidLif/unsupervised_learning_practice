85a33585153bb0d285582c91865e4d3c7ab74f5d
David Lifshitz <davidshani12@gmail.com>
2021-12-10 13:18:15 +0200

    Added extra data and another digit

--- dimension_reduction.py
+++ dimension_reduction.py
@@ -4,18 +4,20 @@ import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.manifold import MDS, LocallyLinearEmbedding, Isomap, SpectralEmbedding
 from sklearn.preprocessing import StandardScaler
-from sklearn.datasets import load_breast_cancer
 from sklearn.datasets import fetch_openml
 
 dimension_reduction_algo_types = ["PCA", "CMDS", "ISO", "LLE", "EigenMaps"]
+dimension_reduction_algo_types_with_neighbors = ["ISO", "LLE", "EigenMaps"]
 
 
 def load_data(base_db):
     mnist = base_db
-    number_9_x_train = mnist.data[mnist.target == "9"][:100]
-    number_9_y_train_tmp = mnist.target[mnist.target == "9"][:100]
-    number_6_x_train = mnist.data[mnist.target == "6"][:100]
-    number_6_y_train_tmp = mnist.target[mnist.target == "6"][:100]
+    number_9_x_train = mnist.data[mnist.target == "9"][:200]
+    number_9_y_train_tmp = mnist.target[mnist.target == "9"][:200]
+    number_6_x_train = mnist.data[mnist.target == "6"][:200]
+    number_6_y_train_tmp = mnist.target[mnist.target == "6"][:200]
+    number_5_x_train = mnist.data[mnist.target == "5"][:200]
+    number_5_y_train_tmp = mnist.target[mnist.target == "5"][:200]
 
     number_9_y_train = np.zeros((number_9_y_train_tmp.shape[0], 1))
     i = 0
@@ -23,22 +25,31 @@ def load_data(base_db):
         number_9_y_train[i] = train_tuple[1]
         i += 1
     number_9_train = np.concatenate([number_9_x_train, number_9_y_train], axis=1)
+
     number_6_y_train = np.zeros((number_6_y_train_tmp.shape[0], 1))
     i = 0
     for train_tuple in number_6_y_train_tmp.iteritems():
         number_6_y_train[i] = train_tuple[1]
         i += 1
     number_6_train = np.concatenate([number_6_x_train, number_6_y_train], axis=1)
-    numbers_train = np.concatenate([number_9_train, number_6_train], axis=0)
+
+    number_5_y_train = np.zeros((number_5_y_train_tmp.shape[0], 1))
+    i = 0
+    for train_tuple in number_5_y_train_tmp.iteritems():
+        number_5_y_train[i] = train_tuple[1]
+        i += 1
+    number_5_train = np.concatenate([number_5_x_train, number_5_y_train], axis=1)
+
+    numbers_train = np.concatenate([number_9_train, number_5_train, number_6_train], axis=0)
 
     data_df = pd.DataFrame(numbers_train)
     data_df.columns = np.append(mnist.feature_names, 'label')
 
     print(f'num features: {len(data_df.columns)}\nSize of the dataframe: {data_df.shape}')
-    return data_df, data_df.columns, [6.0, 9.0]
+    return data_df, data_df.columns, [5.0, 6.0, 9.0]
 
 
-def dim_reduction(data, alg_type, n_components=2):
+def dim_reduction(data, alg_type, n_components=2, k=5):
     if alg_type == "PCA":
         x = data.loc[:, data.columns[:data.shape[1] - 1]].values
         x = StandardScaler().fit_transform(x)  # normalizing the features
@@ -48,13 +59,13 @@ def dim_reduction(data, alg_type, n_components=2):
         model = MDS(n_components=n_components)
         transformed_data = model.fit_transform(data)
     elif alg_type == "ISO":
-        model = Isomap(n_components=n_components)
+        model = Isomap(n_neighbors=k, n_components=n_components)
         transformed_data = model.fit_transform(data)
     elif alg_type == "LLE":
-        model = LocallyLinearEmbedding(n_neighbors=5, n_components=n_components)
+        model = LocallyLinearEmbedding(n_neighbors=k, n_components=n_components)
         transformed_data = model.fit_transform(data)
     elif alg_type == "EigenMaps":
-        model = SpectralEmbedding(n_components=n_components)
+        model = SpectralEmbedding(n_neighbors=k, n_components=n_components)
         transformed_data = model.fit_transform(data)
     else:
         raise Exception("unrecognized algorithm type")
@@ -66,8 +77,7 @@ def dim_reduction(data, alg_type, n_components=2):
 
 
 def visualize(data_df, target_names, label_data, title=f"alg_type on dataset_name", out=""):
-    plt.figure()
-    plt.figure(figsize=(10, 10))
+    figure = plt.figure(figsize=(10, 10))
     plt.xticks(fontsize=12)
     plt.yticks(fontsize=14)
     plt.xlabel('Principal Component - 1', fontsize=20)
@@ -82,15 +92,22 @@ def visualize(data_df, target_names, label_data, title=f"alg_type on dataset_nam
     plt.legend(target_names, prop={'size': 15})
 
     plt.savefig(out)
+    plt.close()
 
 
-def run_single_algo(base_db, alg_type="PCA"):
+def run_single_algo(base_db, alg_type="PCA", k=5):
     data, labels, target_names = load_data(base_db)
-    reduced_data = dim_reduction(data, alg_type)
-    visualize(reduced_data, target_names, data['label'], out=f"{alg_type}.png")
+    reduced_data = dim_reduction(data, alg_type, k=k)
+    if alg_type not in dimension_reduction_algo_types_with_neighbors:
+        k = ""
+    visualize(reduced_data, target_names, data['label'], title=f"{alg_type}_{k} for 6\9 images db"
+              , out=f"{alg_type}_{k}.png")
 
 
 if __name__ == '__main__':
     base_db = fetch_openml('mnist_784')
-    for algo_type in dimension_reduction_algo_types:
-        run_single_algo(base_db, algo_type)
+    run_single_algo(base_db, "PCA")
+    run_single_algo(base_db, "CMDS")
+    for k in [5, 10, 20, 50, 100]:
+        for algo_type in dimension_reduction_algo_types_with_neighbors:
+            run_single_algo(base_db, algo_type, k)
