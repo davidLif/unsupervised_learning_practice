519cb804c72eb28ca8ad09a03c8d6a010a376063
David Lifshitz <davidshani12@gmail.com>
2022-01-07 14:41:40 +0200

    Working stock prediction and visualization

--- hidden_markov_models/hmm.py
+++ hidden_markov_models/hmm.py
@@ -6,33 +6,13 @@ import pandas as pd
 from hmmlearn import hmm  # pip install --upgrade --user hmmlearn
 
 
-def fitHMM(Q, n_states=2, nSamples=0):
+def fitHMM(Q, n_states=2):
     # fit Gaussian HMM to Q
-    Q = np.reshape(Q, [len(Q), 1])
+    Q = np.reshape(Q, [len(Q), 5])
     model = hmm.GaussianHMM(n_components=n_states, n_iter=1000).fit(Q)
     # classify each observation as state 0 or 1
     hidden_states = model.predict(Q)
-    # find parameters of Gaussian HMM
-    mus = np.array(model.means_)
-    sigmas = np.array(np.sqrt(np.array([np.diag(model.covars_[0]), np.diag(model.covars_[1])])))
-    P = np.array(model.transmat_)
-
-    # find log-likelihood of Gaussian HMM
-    logProb = model.score(np.reshape(Q, [len(Q), 1]))
-
-    # generate nSamples from Gaussian HMM
-    samples = model.sample(nSamples)
-
-    # re-organize mus, sigmas and P so that first row is lower mean (if not already)
-    if mus[0] > mus[1]:
-        mus = np.flipud(mus)
-        sigmas = np.flipud(sigmas)
-        P = np.fliplr(np.flipud(P))
-        hidden_states = 1 - hidden_states
-
-    # #check if the model was converged
-    # model.monitor_.converged
-    return hidden_states, mus, sigmas, P, logProb, samples
+    return hidden_states
 
 
 def plotTimeSeries(Q, hidden_states, ylabel, filename):
@@ -40,7 +20,7 @@ def plotTimeSeries(Q, hidden_states, ylabel, filename):
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
-    xs = np.arange(len(Q)) + 1970
+    xs = np.arange(len(Q))
     masks = hidden_states == 0
     ax.scatter(xs[masks], Q[masks], c='r', label='State0')
     masks = hidden_states == 1
@@ -58,20 +38,41 @@ def plotTimeSeries(Q, hidden_states, ylabel, filename):
     return None
 
 
-def visualize_data(df):
+def visualize_data(df, hidden_states_options, file_name):
     plt.figure(figsize=(18, 9))
     plt.plot(range(df.shape[0]), (df['Low'] + df['High']) / 2.0)
-    plt.xticks(range(0, df.shape[0], 500), df['Date'].loc[::500], rotation=45)
-    plt.xlabel('Date', fontsize=18)
+    plt.xticks(range(0, df.shape[0], 500), df['Date_delta'].loc[::500], rotation=45)
+
+    colors_array = ["red", "blue", "green", "yellow", "purple", "orange", "black", "grey", "aqua", "silver", "olive"]
+    for i in hidden_states_options:
+        masks = df['hidden_states'] == i
+        plt.scatter(np.arange(len(df))[masks], ((df['Low'] + df['High']) / 2.0)[masks], c=colors_array[i]
+                    , label='State' + str(i))
+
+    plt.xlabel('Date_delta', fontsize=18)
     plt.ylabel('Mid Price', fontsize=18)
-    plt.show()
+    plt.savefig(file_name)
+    #plt.show()
 
 
 def main():
     # load stocks data
     df = pd.read_csv(os.path.join('Stocks', 'hpq.us.txt'), delimiter=',',
                      usecols=['Date', 'Open', 'High', 'Low', 'Close'])
+    df['Date'] = pd.to_datetime(df['Date'])
+    df['Date_delta'] = (df['Date'] - df['Date'].min()) / np.timedelta64(1, 'D')
+    df = df.drop(columns=['Date'])
+    df['hidden_states'] = 1
 
-    hidden_states, mus, sigmas, P, logProb, samples = fitHMM(df, nSamples=100)
-    plt.switch_backend('agg')  # turn off display when running with Cygwin
-    plotTimeSeries(df, hidden_states, 'MidPrice', 'StateTseries_Log.png')
+    for n_states in [2, 4, 6, 8]:
+        df = df.drop(columns=['hidden_states'])
+        hidden_states = fitHMM(df, n_states)
+
+        df['hidden_states'] = hidden_states
+        #plt.switch_backend('agg')  # turn off display when running with Cygwin
+        samples = df.sample(200)
+        visualize_data(samples, range(n_states), "graph_for_{0}_hidden_states.svg".format(n_states))
+
+
+if __name__ == "__main__":
+    main()
