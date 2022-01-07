a7d9e09d47d018a48c253f3c752e2df672406442
David Lifshitz <davidshani12@gmail.com>
2022-01-07 15:16:46 +0200

    Use dim reduction to better visualaze the data

--- hidden_markov_models/hmm.py
+++ hidden_markov_models/hmm.py
@@ -4,6 +4,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 from hmmlearn import hmm  # pip install --upgrade --user hmmlearn
+from sklearn.manifold import MDS
 
 
 def fitHMM(Q, n_states=2):
@@ -40,17 +41,18 @@ def plotTimeSeries(Q, hidden_states, ylabel, filename):
 
 def visualize_data(df, hidden_states_options, file_name):
     plt.figure(figsize=(18, 9))
-    plt.plot(range(df.shape[0]), (df['Low'] + df['High']) / 2.0)
+    plt.plot(range(df.shape[0]), (df['principle_cmp1']))
     plt.xticks(range(0, df.shape[0], 500), df['Date_delta'].loc[::500], rotation=45)
 
-    colors_array = ["red", "blue", "green", "yellow", "purple", "orange", "black", "grey", "aqua", "silver", "olive"]
+    colors_array = ["red", "blue", "green", "yellow", "purple", "orange", "black", "grey", "aqua"
+        , "silver", "olive", "pink", "darkseagreen"]
     for i in hidden_states_options:
         masks = df['hidden_states'] == i
-        plt.scatter(np.arange(len(df))[masks], ((df['Low'] + df['High']) / 2.0)[masks], c=colors_array[i]
+        plt.scatter(np.arange(len(df))[masks], (df['principle_cmp1'])[masks], c=colors_array[i]
                     , label='State' + str(i))
 
     plt.xlabel('Date_delta', fontsize=18)
-    plt.ylabel('Mid Price', fontsize=18)
+    plt.ylabel('principle_cmp1', fontsize=18)
     plt.savefig(file_name)
     #plt.show()
 
@@ -62,16 +64,25 @@ def main():
     df['Date'] = pd.to_datetime(df['Date'])
     df['Date_delta'] = (df['Date'] - df['Date'].min()) / np.timedelta64(1, 'D')
     df = df.drop(columns=['Date'])
-    df['hidden_states'] = 1
 
-    for n_states in [2, 4, 6, 8]:
-        df = df.drop(columns=['hidden_states'])
+    # Dim reduction to
+    df = df.sample(3000)  # My computer doesn't have enough RAM for more than that.
+    model = MDS(n_components=1)
+    transformed_data = model.fit_transform(df[['Open', 'High', 'Low', 'Close']])
+    transformed_data_df = pd.DataFrame(data=transformed_data
+                                       , columns=['principle_cmp1'])
+    transformed_data_df['Date_delta'] = df['Date_delta']
+    transformed_data_df['hidden_states'] = 1
+
+    for n_states in [13]:
+        transformed_data_df.drop(columns=['hidden_states'])
         hidden_states = fitHMM(df, n_states)
 
-        df['hidden_states'] = hidden_states
+        transformed_data_df['hidden_states'] = hidden_states
+
         #plt.switch_backend('agg')  # turn off display when running with Cygwin
-        samples = df.sample(200)
-        visualize_data(samples, range(n_states), "graph_for_{0}_hidden_states.svg".format(n_states))
+        samples = transformed_data_df.sample(200)
+        visualize_data(samples, range(n_states), "graph_with_dm_{0}_hidden_states.svg".format(n_states))
 
 
 if __name__ == "__main__":
