dcf103a9f955c8f830bab85e6280b40f92092591
einavshe <einavshen@gmail.com>
2022-02-28 19:31:51 +0200

    MCA

--- FinalProject/main.py
+++ FinalProject/main.py
@@ -5,7 +5,8 @@ from evaluator import Evaluator, stats_dir
 
 
 def load_data():
-    data_df = pd.read_csv("./DATA/USCensus1990.clean_data.csv", dtype=np.int)
+    # data_df = pd.read_csv("./DATA/USCensus1990.clean_data.csv", dtype=np.int)
+    data_df = pd.read_csv("./DATA/USCensus1990.MCA_20features.csv", dtype=np.float)
     # train_data, test_data = train_test_split(data_df, test_size=0.2,
     #                                          random_state=0)  # choose data for this iteration
     return data_df.drop(columns="Unnamed: 0")#.sample(100, random_state=0)  # train_data, test_data
